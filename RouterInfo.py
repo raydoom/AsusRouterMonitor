7763d1d9c2b8f6c3b5e21e68d1c7a42d5dd004a7
raydoom <raydoom@msn.com>
2024-05-13 09:41:13 +0800

    Fix Uptime json error
    [root@rl8-18 asus]# python3.11 ShowRouterInfo.py
    {
    "uptime":"Mon, 13 May 2024 09:28:43 +0800(63921 secs since boot)"
    }
    
    Traceback (most recent call last):
      File "/root/asus/ShowRouterInfo.py", line 5, in <module>
        print("Uptime    : " + str(ri.get_uptime()))
                                   ^^^^^^^^^^^^^^^
      File "/root/asus/RouterInfo.py", line 75, in get_uptime
        return json.loads('{' + '"since":"{}", "uptime":"{}"'.format(since, up) + '}')
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/usr/lib64/python3.11/json/__init__.py", line 346, in loads
        return _default_decoder.decode(s)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/usr/lib64/python3.11/json/decoder.py", line 337, in decode
        obj, end = self.raw_decode(s, idx=_w(s, 0).end())
                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/usr/lib64/python3.11/json/decoder.py", line 353, in raw_decode
        obj, end = self.scan_once(s, idx)
                   ^^^^^^^^^^^^^^^^^^^^^^
    json.decoder.JSONDecodeError: Expecting ',' delimiter: line 1 column 12 (char 11)

--- RouterInfo.py
+++ RouterInfo.py
@@ -69,7 +69,7 @@ class RouterInfo:
         :returns: JSON with last boot time and uptime in seconds
         """
         r = self.__get('uptime()')
-        since = r.partition(':')[2].partition('(')[0]
+        since = r.partition(':')[2].partition('(')[0][1:]
         up = r.partition('(')[2].partition(' ')[0]
         return json.loads('{' + '"since":"{}", "uptime":"{}"'.format(since, up) + '}')
 
